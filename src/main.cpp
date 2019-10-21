cce2b627bb68748e3d9c5ba92e1bcafe4f2b4a28
Kirill Surkov <surkovk20@yandex.ru>
2019-10-21 04:28:54 +0300

    Modify initialization

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,17 @@
 #include "precompiled.hpp"
+#include "v8_platform.hpp"
+#include "physics_oimo.hpp"
 #include "listener.hpp"
 #include "core.hpp"
-#include "v8_platform.hpp"
 
 #define PORT 34851
 #define THREADS 4
+#define TIME_STEP (1.0f / 60.0f)
 
 int main() {
     v8_platform_t v8_platform;
-    physics_t physics(v8_platform.create_isolate());
-    core_t core(physics);
+    auto physics = std::make_shared<physics_oimo_t>(v8_platform.create_isolate(), TIME_STEP);
+    core_t core(physics, TIME_STEP);
     boost::asio::io_context io{THREADS};
     std::make_shared<listener_t>(io, core, boost::asio::ip::tcp::endpoint(boost::asio::ip::make_address("0.0.0.0"), PORT))->run();
     std::vector<std::thread> threads;
