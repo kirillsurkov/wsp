75a8ddfdce5113923fa250447e2049a16ca8037b
Kirill Surkov <surkovk20@yandex.ru>
2019-10-21 04:28:27 +0300

    Move v8_lock_t to v8_platform_t

--- include/v8_platform.hpp
+++ include/v8_platform.hpp
@@ -4,6 +4,22 @@
 #include <libplatform/libplatform.h>
 
 class v8_platform_t {
+public:
+    class v8_lock_t {
+    private:
+        v8::Locker m_lock;
+        v8::Isolate::Scope m_isolate_scope;
+        v8::HandleScope m_handle_scope;
+
+    public:
+        v8_lock_t(v8::Isolate* isolate) :
+            m_lock(isolate),
+            m_isolate_scope(isolate),
+            m_handle_scope(isolate)
+        {
+        }
+    };
+
 private:
     std::unique_ptr<v8::Platform> m_platform;
     v8::Isolate::CreateParams m_params;
