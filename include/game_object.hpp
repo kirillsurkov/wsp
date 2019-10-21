1cf315f391c2d87207965ff6a33897c21f6715a2
Kirill Surkov <surkovk20@yandex.ru>
2019-10-21 04:26:03 +0300

    Separate body from physics_object_t

--- include/game_object.hpp
+++ include/game_object.hpp
@@ -11,8 +11,8 @@ public:
     game_object_t(int id);
     virtual ~game_object_t();
 
-    virtual glm::vec3 get_position() const = 0;
-    virtual glm::vec4 get_rotation() const = 0;
+    virtual const glm::vec3& get_position() const = 0;
+    virtual const glm::vec4& get_rotation() const = 0;
 
     int get_id() const;
 };
