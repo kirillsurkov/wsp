12d5b9a9d39589b53f0e602e9885ce53e7ffcd27
Kirill Surkov <surkovk20@yandex.ru>
2019-10-24 05:08:30 +0300

    Update client

--- web/game_networking.js
+++ web/game_networking.js
@@ -4,9 +4,106 @@ const Protocol = {
 };
 
 const msg_in = {
-    login: 1,
-    body_create: 4,
-    body_state: 5
+    login: {
+        id: 1,
+        create_binary: function(data) {
+            return {
+                frame: data.frame
+            };
+        }
+    },
+    body_create: {
+        id: 4,
+        create_binary: function(data) {
+            console.log("CREATE");
+            console.log(new Uint8Array(data.data.buffer));
+            let res = {
+                id: data.data.getUint16(data.offset, true),
+                moving: !!data.data.getUint8(data.offset + 2),
+                position: [
+                    data.data.getFloat32(data.offset + 3, true),
+                    data.data.getFloat32(data.offset + 7, true),
+                    data.data.getFloat32(data.offset + 11, true)
+                ],
+                rotation: [
+                    data.data.getFloat32(data.offset + 15, true),
+                    data.data.getFloat32(data.offset + 19, true),
+                    data.data.getFloat32(data.offset + 23, true),
+                    data.data.getFloat32(data.offset + 27, true)
+                ],
+                rotation_factor: [
+                    data.data.getFloat32(data.offset + 31, true),
+                    data.data.getFloat32(data.offset + 35, true),
+                    data.data.getFloat32(data.offset + 39, true)
+                ],
+                shapes: []
+            };
+            data.offset += 43;
+            let shapes_count = data.data.getUint8(data.offset++);
+            for (let i = 0; i < shapes_count; i++) {
+                res.shapes.push({
+                    type: data.data.getUint8(data.offset),
+                    position: [
+                        data.data.getFloat32(data.offset + 1, true),
+                        data.data.getFloat32(data.offset + 5, true),
+                        data.data.getFloat32(data.offset + 9, true)
+                    ],
+                    rotation: [
+                        data.data.getFloat32(data.offset + 13, true),
+                        data.data.getFloat32(data.offset + 17, true),
+                        data.data.getFloat32(data.offset + 21, true),
+                        data.data.getFloat32(data.offset + 25, true)
+                    ],
+                    size: [
+                        data.data.getFloat32(data.offset + 29, true),
+                        data.data.getFloat32(data.offset + 33, true),
+                        data.data.getFloat32(data.offset + 37, true)
+                    ]
+                });
+                data.offset += 41;
+            }
+            return res;
+        }
+    },
+    body_state: {
+        id: 5,
+        create_binary: function(data) {
+            let res = {
+                objects: []
+            };
+            let objects_count = data.data.getUint32(data.offset, true);
+            data.offset += 4;
+            for (let i = 0; i < objects_count; i++) {
+                res.objects.push({
+                    id: data.data.getUint16(data.offset, true),
+                    sleeping: !!data.data.getUint8(data.offset + 2),
+                    position: [
+                        data.data.getFloat32(data.offset + 3, true),
+                        data.data.getFloat32(data.offset + 7, true),
+                        data.data.getFloat32(data.offset + 11, true)
+                    ],
+                    rotation: [
+                        data.data.getFloat32(data.offset + 15, true),
+                        data.data.getFloat32(data.offset + 19, true),
+                        data.data.getFloat32(data.offset + 23, true),
+                        data.data.getFloat32(data.offset + 27, true)
+                    ],
+                    linear_speed: [
+                        data.data.getFloat32(data.offset + 31, true),
+                        data.data.getFloat32(data.offset + 35, true),
+                        data.data.getFloat32(data.offset + 39, true)
+                    ],
+                    angular_speed: [
+                        data.data.getFloat32(data.offset + 43, true),
+                        data.data.getFloat32(data.offset + 47, true),
+                        data.data.getFloat32(data.offset + 51, true)
+                    ]
+                });
+                data.offset += 55;
+            }
+            return res;
+        }
+    }
 };
 
 const msg_out = {
@@ -25,8 +122,39 @@ const packets_out = {
     }
 }
 
-
 function GameNetworking(game, protocol) {
+    // TODO: Make better
+    let parse_messages = {}
+    parse_messages[Protocol.json] = function(data) {
+        return JSON.parse(new TextDecoder("UTF-8").decode(data));
+    };
+    parse_messages[Protocol.binary] = function(data) {
+        let info = {
+            data: data,
+            offset: 0
+        };
+        let messages = [];
+        let count = info.data.getUint8(info.offset++);
+        console.log("count: " + count);
+        a=[];
+        for (let i = 0; i < count; i++) {
+            let type = info.data.getUint8(info.offset);
+            let frame = info.data.getUint32(info.offset + 1, true);
+            info.offset += 5;
+            let message_info = Object.values(msg_in).find(message => message.id == type);
+            if (message_info) {
+                let message = message_info.create_binary(info);
+                message.frame = frame;
+                messages.push({type: type, data: message});
+                a.push(i);
+            } else {
+                console.log(type + " not found at i = " + i);
+            }
+        }
+        console.log(a);
+        return messages;
+    };
+
     let socket;
 
     let on_connect = () => {
@@ -34,33 +162,27 @@ function GameNetworking(game, protocol) {
     };
 
     let on_message = message => {
-        let messages = [];
-        switch (protocol) {
-            case Protocol.json: {
-                messages = JSON.parse(message.data);
-                break;
-            }
-            case Protocol.binary: {
-                break;
-            }
-        }
+        let data = new DataView(message.data);
+        let messages = parse_messages[protocol](data);
+        console.log("received messages");
         console.log(messages);
         for (let message of messages) {
             switch (message.type) {
-                case msg_in.login: {
+                case msg_in.login.id: {
                     game.set_frame(message.data.frame - 5);
                     break;
                 }
-                case msg_in.body_create: {
+                case msg_in.body_create.id: {
                     game.push_action(Action.body_create, message.data);
                     break;
                 }
-                case msg_in.body_state: {
+                case msg_in.body_state.id: {
                     game.push_action(Action.body_state, message.data);
                     break;
                 }
             }
         }
+        console.log("on_message done");
     };
 
     let binary_buffer = new DataView(new ArrayBuffer(102400)); // 100K
@@ -117,10 +239,11 @@ function GameNetworking(game, protocol) {
                     buffer = new ArrayBuffer(offset);
                     let buffer_view = new Uint8Array(buffer);
                     buffer_view.set(new Uint8Array(binary_buffer.buffer, 0, offset));
+                    console.log("sending bytes");
+                    console.log(buffer_view);
                     break;
                 }
             }
-            console.log(buffer);
             socket.send(buffer);
             game.clear_packets();
         }
